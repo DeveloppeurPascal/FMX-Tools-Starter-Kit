bb1e55d7cba064009735fcce9ef8d72cc05f304f
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-09 13:07:46 +0200

    added XML doc commentds and implemented some methods

--- src/_MainFormAncestor.pas
+++ src/_MainFormAncestor.pas
@@ -52,7 +52,8 @@ uses
   FMX.Menus,
   uDMAboutBox,
   fToolsLanguagesDialog,
-  Olf.FMX.AboutDialogForm;
+  Olf.FMX.AboutDialogForm,
+  uDocumentsAncestor;
 
 type
   T__MainFormAncestor = class(T__TFormAncestor)
@@ -111,12 +112,16 @@ type
     procedure actCloseDocumentExecute(Sender: TObject);
     procedure actCloseAllDocumentsExecute(Sender: TObject);
     procedure actRecentFilesOptionsExecute(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
   private
     FonGetLanguageName: TOnGetLanguageName;
     FOnAboutBoxTranslateTexts: TOnAboutBoxTranslateTexts;
+    FCurrentDocument: TDocumentAncestor;
     procedure SetonGetLanguageName(const Value: TOnGetLanguageName);
     procedure SetOnAboutBoxTranslateTexts(const Value
       : TOnAboutBoxTranslateTexts);
+    procedure SetCurrentDocument(const Value: TDocumentAncestor);
   protected
     /// <summary>
     /// Show/hide TMainMenu items depending on there sub menus items visibility
@@ -127,7 +132,7 @@ type
     /// Show/hide a TMenuItem depending on its sub menus items visibility
     /// </summary>
     function RefreshMenuItemsVisibility(const MenuItem: TMenuItem;
-      const FirstLevel: boolean): boolean; overload; virtual;
+      const FirstLevel: Boolean): Boolean; overload; virtual;
     /// <summary>
     /// Called by the actQuit action used for File/Quit menu option.
     /// </summary>
@@ -153,14 +158,14 @@ type
     /// Called by the actDocumentOptions action used for Document/Options menu option.
     /// </summary>
     /// <remarks>
-    /// Nothing is done by default. Overrire this method if you want to do something.
+    /// Nothing is done by default. Override this method if you want to do something.
     /// </remarks>
     procedure DoDocumentOptionsAction(Sender: TObject); virtual; abstract;
     /// <summary>
     /// Called by the actLanguages action used for Tools/Languages menu option.
     /// </summary>
     /// <remarks>
-    /// Nothing is done by default. Overrire this method if you want to do something.
+    /// Nothing is done by default. Override this method if you want to do something.
     /// </remarks>
     procedure DoLanguageChangeAction(Sender: TObject); virtual;
     /// <summary>
@@ -174,7 +179,7 @@ type
     /// Called by the actToolsOptions action used for Tools/Options menu option.
     /// </summary>
     /// <remarks>
-    /// Nothing is done by default. Overrire this method if you want to do something.
+    /// Nothing is done by default. Override this method if you want to do something.
     /// </remarks>
     procedure DoToolsOptionsAction(Sender: TObject); virtual; abstract;
     /// <summary>
@@ -198,17 +203,88 @@ type
     /// </remarks>
     function DoAboutBoxTranslateTexts(const Language: string;
       const TxtID: TOlfAboutDialogTxtID): string; virtual;
+    /// <summary>
+    /// Called when we need to create a new document
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change the document type and use your descendant type
+    /// </remarks>
     procedure DoDocumentNewAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we need to open a document
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change the document type and use your descendant type
+    /// </remarks>
     procedure DoDocumentOpenAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we need to save a document
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoDocumentSaveAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we need to save a document as an other file (or a new one)
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoDocumentSaveAsAction(Sender: TObject); virtual;
-    procedure DoDocumentCloseAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we need to close a document
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
+    procedure DoDocumentCloseAction(Sender: TObject;
+      const Proc: TProc = nil); virtual;
+    /// <summary>
+    /// Called when we need to save all opened (and changed) documents
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoSaveAllAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we need to open a recent opened document
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoOpenPreviousAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we want to change current document and use an other opened one
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoGoToDocAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we need to close all documents
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoCloseAllAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Called when we open the options dialog to change "recent files" properties
+    /// </summary>
+    /// <remarks>
+    /// Override this method to change default behaviour
+    /// </remarks>
     procedure DoRecentDocumentsOptionsAction(Sender: TObject); virtual;
+    /// <summary>
+    /// Returns a document instance.
+    /// Override it in your main form descendant to create an instance of the good class (yours)
+    /// </summary>
+    function GetNewDoc: TDocumentAncestor; virtual;
   public
+    /// <summary>
+    /// Current opened document
+    /// </summary>
+    property CurrentDocument: TDocumentAncestor read FCurrentDocument
+      write SetCurrentDocument;
     /// <summary>
     /// Called in the Tools/Languages dialog to get the language name for an ISO code.
     /// </summary>
@@ -219,6 +295,10 @@ type
     /// </summary>
     property OnAboutBoxTranslateTexts: TOnAboutBoxTranslateTexts
       read FOnAboutBoxTranslateTexts write SetOnAboutBoxTranslateTexts;
+    /// <summary>
+    /// Class constructor
+    /// </summary>
+    constructor Create(AOwner: TComponent); override;
     /// <summary>
     /// Execute some code after the form instance construction
     /// </summary>
@@ -243,7 +323,8 @@ uses
   u_urlOpen,
   uConsts,
   uStyleManager,
-  fToolsStylesDialog;
+  fToolsStylesDialog,
+  System.IOUtils;
 
 procedure T__MainFormAncestor.actAboutExecute(Sender: TObject);
 begin
@@ -343,6 +424,14 @@ begin
 {$ENDIF}
 end;
 
+constructor T__MainFormAncestor.Create(AOwner: TComponent);
+begin
+  inherited;
+  FonGetLanguageName := nil;
+  FOnAboutBoxTranslateTexts := nil;
+  FCurrentDocument := nil;
+end;
+
 procedure T__MainFormAncestor.DoAboutAction(Sender: TObject);
 begin
   if assigned(OnAboutBoxTranslateTexts) then
@@ -361,32 +450,80 @@ end;
 
 procedure T__MainFormAncestor.DoCloseAllAction(Sender: TObject);
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  // TODO -oDeveloppeurPascal : à compléter
 end;
 
-procedure T__MainFormAncestor.DoDocumentCloseAction(Sender: TObject);
+procedure T__MainFormAncestor.DoDocumentCloseAction(Sender: TObject;
+const Proc: TProc);
+var
+  i: integer;
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  // TODO -oDeveloppeurPascal : à compléter
+
+  for i := mnuWindows.ItemsCount - 1 downto 0 do
+    if (FCurrentDocument = mnuWindows.Items[i].TagObject) then
+      mnuWindows.Items[i].Free;
+
+  freeandnil(FCurrentDocument);
+
+  if assigned(Proc) then
+    tthread.forcequeue(nil,
+      procedure
+      begin
+        Proc;
+      end);
 end;
 
 procedure T__MainFormAncestor.DoDocumentNewAction(Sender: TObject);
+var
+  NewDoc: TDocumentAncestor;
+  MI: TMenuItem;
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  NewDoc := GetNewDoc;
+  // TODO -oDeveloppeurPascal : ajouter le document à la liste des documents
+
+  MI := TMenuItem.Create(self);
+  MI.Parent := mnuWindows;
+  MI.Text := NewDoc.FileName;
+  MI.TagObject := NewDoc;
+  MI.OnClick := DoGoToDocAction;
+  mnuWindows.Visible := RefreshMenuItemsVisibility(mnuWindows, true);
+
+  CurrentDocument := NewDoc;
 end;
 
 procedure T__MainFormAncestor.DoDocumentOpenAction(Sender: TObject);
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  // TODO -oDeveloppeurPascal : choisir un fichier
+  // TODO -oDeveloppeurPascal : ajouter une option de menu dans les "Windows" pour ce document
+  // TODO -oDeveloppeurPascal : ajouter ce document aux fichiers récents
 end;
 
 procedure T__MainFormAncestor.DoDocumentSaveAction(Sender: TObject);
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  if FCurrentDocument.FileName.IsEmpty then
+    DoDocumentSaveAsAction(Sender)
+  else
+    FCurrentDocument.SaveToFile;
 end;
 
 procedure T__MainFormAncestor.DoDocumentSaveAsAction(Sender: TObject);
+var
+  i: integer;
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  // TODO -oDeveloppeurPascal : choisir le dossier et nom de fichier de stockage
+  // TODO -oDeveloppeurPascal : faire l'enregistrement du fichier
+  // TODO -oDeveloppeurPascal : ajouter le nouveau nom/chemin aux fichiers récents
+
+  for i := 0 to mnuWindows.ItemsCount - 1 do
+    if (FCurrentDocument = mnuWindows.Items[i].TagObject) then
+    begin
+      mnuWindows.Items[i].Text := FCurrentDocument.FileName;
+      break;
+    end;
+
+  TAboutBox.Current.OlfAboutDialog1.MainFormCaptionPrefix :=
+    FCurrentDocument.FileName + ' - ';
 end;
 
 function T__MainFormAncestor.DoGetLanguageName(const ISOCode: string): string;
@@ -397,14 +534,17 @@ end;
 
 procedure T__MainFormAncestor.DoGoToDocAction(Sender: TObject);
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  if assigned(Sender) and (Sender is TMenuItem) and
+    assigned((Sender as TMenuItem).TagObject) and
+    ((Sender as TMenuItem).TagObject is TDocumentAncestor) then
+    CurrentDocument := (Sender as TMenuItem).TagObject as TDocumentAncestor;
 end;
 
 procedure T__MainFormAncestor.DoLanguageChangeAction(Sender: TObject);
 var
   f: TfrmToolsLanguagesDialog;
 begin
-  f := TfrmToolsLanguagesDialog.create(self);
+  f := TfrmToolsLanguagesDialog.Create(self);
   if assigned(onGetLanguageName) then
     f.onGetLanguageName := onGetLanguageName
   else
@@ -421,8 +561,18 @@ begin
 end;
 
 procedure T__MainFormAncestor.DoOpenPreviousAction(Sender: TObject);
+var
+  NewDoc: TDocumentAncestor;
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  if assigned(Sender) and (Sender is TMenuItem) and
+    (not(Sender as TMenuItem).TagString.IsEmpty) and
+    TFile.Exists((Sender as TMenuItem).TagString) then
+  begin
+    // TODO -oDeveloppeurPascal : check if this file is already opened and go to it
+    NewDoc := GetNewDoc;
+    NewDoc.LoadFromFile((Sender as TMenuItem).TagString);
+    CurrentDocument := NewDoc;
+  end;
 end;
 
 procedure T__MainFormAncestor.DoQuitAction(Sender: TObject);
@@ -432,19 +582,19 @@ end;
 
 procedure T__MainFormAncestor.DoRecentDocumentsOptionsAction(Sender: TObject);
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  // TODO -oDeveloppeurPascal : à compléter
 end;
 
 procedure T__MainFormAncestor.DoSaveAllAction(Sender: TObject);
 begin
-  // TODO -oDeveloppeurPacal : à compléter
+  // TODO -oDeveloppeurPascal : boucler sur tous les documents ouvert et enregistrer ceux qui ne le sont pas
 end;
 
 procedure T__MainFormAncestor.DoStyleChangeAction(Sender: TObject);
 var
   f: TfrmToolsStylesDialog;
 begin
-  f := TfrmToolsStylesDialog.create(self);
+  f := TfrmToolsStylesDialog.Create(self);
 {$IF Defined(IOS) or Defined(ANDROID)}
   f.show;
 {$ELSE}
@@ -461,11 +611,33 @@ begin
   if not CSupportURL.IsEmpty then
     url_Open_In_Browser(CSupportURL)
   else
-    raise exception.create('Missing support website URL.');
+    raise exception.Create('Missing support website URL.');
+end;
+
+procedure T__MainFormAncestor.FormClose(Sender: TObject;
+var Action: TCloseAction);
+begin
+  DoCloseAllAction(Sender);
+end;
+
+procedure T__MainFormAncestor.FormCloseQuery(Sender: TObject;
+var CanClose: Boolean);
+begin
+  // TODO -oDeveloppeurPascal : tester si des documents sont ouverts et non enregistrés
+end;
+
+function T__MainFormAncestor.GetNewDoc: TDocumentAncestor;
+begin
+{$IFDEF RELEASE}
+  raise exception.Create
+    ('Don''t call ancestor GetNewDoc, override it with your own document class descendant !');
+{$ELSE}
+  result := TDocumentAncestor.Create;
+{$ENDIF}
 end;
 
 function T__MainFormAncestor.RefreshMenuItemsVisibility(const MenuItem
-  : TMenuItem; const FirstLevel: boolean): boolean;
+  : TMenuItem; const FirstLevel: Boolean): Boolean;
 var
   i: integer;
 begin
@@ -489,6 +661,61 @@ begin
   end
   else
     result := false;
+end;
+
+procedure T__MainFormAncestor.SetCurrentDocument(const Value
+  : TDocumentAncestor);
+var
+  NewDoc: TDocumentAncestor;
+  i: integer;
+begin
+  if (FCurrentDocument <> Value) then
+  begin
+    if assigned(FCurrentDocument) and (CDocumentsMode <> TDocumentsMode.Multi)
+    then
+    begin
+      NewDoc := Value;
+      DoDocumentCloseAction(nil,
+        procedure
+        begin
+          CurrentDocument := NewDoc;
+        end);
+    end
+    else
+    begin
+      if assigned(FCurrentDocument) then
+      begin
+        for i := 0 to mnuWindows.ItemsCount - 1 do
+          if (mnuWindows.Items[i].IsChecked) and
+            (FCurrentDocument = mnuWindows.Items[i].TagObject) then
+          begin
+            mnuWindows.Items[i].IsChecked := false;
+            break;
+          end;
+      end;
+
+      FCurrentDocument := Value;
+      if assigned(FCurrentDocument) then
+      begin
+        if FCurrentDocument.HasChanged then
+          TAboutBox.Current.OlfAboutDialog1.MainFormCaptionPrefix :=
+            FCurrentDocument.FileName + '(*) - '
+        else
+          TAboutBox.Current.OlfAboutDialog1.MainFormCaptionPrefix :=
+            FCurrentDocument.FileName + ' - ';
+        for i := 0 to mnuWindows.ItemsCount - 1 do
+          if (FCurrentDocument = mnuWindows.Items[i].TagObject) then
+          begin
+            mnuWindows.Items[i].IsChecked := true;
+            break;
+          end;
+      end
+      else
+        TAboutBox.Current.OlfAboutDialog1.MainFormCaptionPrefix := '';
+
+      TCurrentDocumentChangedMessage.Broadcast(FCurrentDocument);
+    end;
+  end;
 end;
 
 procedure T__MainFormAncestor.SetOnAboutBoxTranslateTexts
@@ -579,8 +806,8 @@ begin
 {$ELSE}
 {$IFDEF MACOS}
   mnuFileQuit.Visible := false;
-  mnuHelpAbout.parent := mnuMacOS;
-  mnuTools.parent := mnuMacOS;
+  mnuHelpAbout.Parent := mnuMacOS;
+  mnuTools.Parent := mnuMacOS;
 {$ENDIF}
   mnuToolsLanguage.Visible := CShowToolsLanguagesMenuItem;
   mnuToolsStyle.Visible := CShowToolsStylesMenuItem;
