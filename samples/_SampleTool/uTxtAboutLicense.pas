ac44b0aa6727355f90b93e1b5182c0a205b2e130
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-26 20:07:18 +0100

    added a personalized EULA sample for this project

---
+++ samples/_SampleTool/uTxtAboutLicense.pas
@@ -0,0 +1,72 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// FMX Tools Starter Kit
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://fmxtoolsstarterkit.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/FMX-Tools-Starter-Kit
+///
+/// ***************************************************************************
+/// File last update : 2024-08-28T12:18:58.000+02:00
+/// Signature : bd2770ed78b9d8977c24a60d7ae02be189f1015e
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutLicense;
+
+interface
+
+function GetTxtAboutLicense(const Language: string;
+  const Recursif: boolean = false): string;
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const
+  CTxtEN = 'Your end user license agreement.';
+  CTxtFR = 'Votre licence d''utilisateur final.';
+  CTxtIT = 'La vostra licenza per l''utente finale.';
+
+function GetTxtAboutLicense(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+  else if (lng = 'it') then // Italy
+    result := CTxtIT
+  else if not Recursif then
+    result := GetTxtAboutLicense(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow license for language "' + Language + '".');
+end;
+
+end.
