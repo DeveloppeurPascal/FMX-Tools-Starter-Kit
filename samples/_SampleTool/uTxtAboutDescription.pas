6e3f3e34e20b6ee62d2069ce8227dd375804c92f
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-26 20:06:59 +0100

    added a personalized description for this project

---
+++ samples/_SampleTool/uTxtAboutDescription.pas
@@ -0,0 +1,73 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// FMX Tools Starter Kit
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://fmxtoolsstarterkit.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/FMX-Tools-Starter-Kit
+///
+/// ***************************************************************************
+/// File last update : 2024-08-28T12:19:24.000+02:00
+/// Signature : bb1284c79b6f8a793c5705af597b6bed59268b69
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutDescription;
+
+interface
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean = false): string;
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const
+  CTxtEN = 'Project description in English.';
+  CTxtFR = 'Description du projet en français.';
+  CTxtIT = 'Descrizione del progetto in francese.';
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+  else if (lng = 'it') then // Italy
+    result := CTxtIT
+  else if not Recursif then
+    result := GetTxtAboutDescription(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow description for language "' +
+      Language + '".');
+end;
+
+end.
