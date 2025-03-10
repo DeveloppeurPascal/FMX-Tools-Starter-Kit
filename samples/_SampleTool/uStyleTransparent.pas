2f1f0a6ed441bc6a1fb9b57e254a11a0dd6b3655
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-03-10 21:13:18 +0100

    added Transparent style for Windows and macOS

---
+++ samples/_SampleTool/uStyleTransparent.pas
@@ -0,0 +1,76 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// FMX Tools Starter Kit
+///
+/// Copyright 2024-2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://fmxtoolsstarterkit.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/FMX-Tools-Starter-Kit
+///
+/// ***************************************************************************
+/// File last update : 2025-03-10T21:01:06.000+01:00
+/// Signature : 08603032f2a8240bc33f0984f899f1828215a080
+/// ***************************************************************************
+/// </summary>
+
+unit uStyleTransparent;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  _StyleContainerAncestor,
+  FMX.Types,
+  FMX.Controls,
+  uStyleManager;
+
+type
+  TdmStyleTransparent = class(T__StyleContainerAncestor)
+  private
+  public
+    class function GetStyleName: string; override;
+    class function GetStyleType: TProjectStyleType; override;
+  end;
+
+var
+  dmStyleTransparent: TdmStyleTransparent;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+{ TdmStyleTransparent }
+
+class function TdmStyleTransparent.GetStyleName: string;
+begin
+  result := 'Transparent';
+end;
+
+class function TdmStyleTransparent.GetStyleType: TProjectStyleType;
+begin
+  result := TProjectStyleType.dark;
+end;
+
+initialization
+
+TdmStyleTransparent.Initialize;
+
+end.
